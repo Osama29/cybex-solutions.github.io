076f93a4c53429d4eb2b766957a1596eb4acb452
Osama29 <53341547+Osama29@users.noreply.github.com>
2023-02-13 12:26:09 +0800

    Email Forwarding Demo

---
+++ submit-form.php
@@ -0,0 +1,22 @@
+<!-- submit-form.php -->
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // send email with the user's information
+  $to = "your-email-address@example.com";
+  $subject = "New Contact Us Submission";
+  $body = "Name: $name\n\nEmail: $email\n\nMessage:\n$message";
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $body, $headers)) {
+    // success message
+    echo "Your message was sent successfully!";
+  } else {
+    // error message
+    echo "There was an error sending your message. Please try again later.";
+  }
+}
+?>
